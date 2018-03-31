1a5e3e92aa572bbba9bb228cbe812d9a806ed1fb
Dzokaredevil <dima.voytovich@mail.ru>
2018-03-31 04:46:47 +0300

    Print a single integer — the least possible difference the teacher can obtain.

---
+++ Puzzles.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<time.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+	ll a, b, mini = 100000, c[80] = { 0 };
+	cin >> a >> b;
+	for (int i = 0; i < b; i++)
+		cin >> c[i];
+	sort(c, c + b);
+	for (int i = 0; i < b - a + 1; i++)
+		mini = min(mini, c[a - 1 + i] - c[i]);
+	cout << mini;
+	return 0;
+}
