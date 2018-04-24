259121ee352c5c2e94b6ed5698205bf83b4f19bb
Dzokaredevil <dima.voytovich@mail.ru>
2018-04-24 03:50:46 +0300

    Print a single integer — the number of hours Vasily can light up the room for.

---
+++ New Year Candles.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, c, i, j;
+    int b;
+    cin >> a;
+    cin >> b;
+    int sum = a;
+
+    int p = a;
+    int y = a;
+    while(y >= b){
+        a = y/b;
+        y = y%b;
+        sum = sum+a;
+        y = y+a;
+    }
+    cout << sum;
+}
