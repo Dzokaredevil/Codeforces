172a313e821e2ff5770d8aeb705c038530c7149c
Dzokaredevil <dima.voytovich@mail.ru>
2018-04-28 03:26:05 +0300

    Output two integers — the row and the column of a cell that belongs to the maximum possible number of different locations of the ship.
    If there are multiple answers, output any of them. In particular, if no ship can be placed on the field, you can output any cell.

---
+++ Battleship.py
@@ -0,0 +1,29 @@
+n, k = map(int, input().split())
+m = []
+for i in range(n):
+    u = str(input())
+    m.append(u)
+v = [[0] * n for _ in range(n)]
+for x in range(n - k + 1):
+    for y in range(n):
+        s = m[y][x:x+k]
+        if not '#' in s:
+            for i in range(k):
+                v[y][x + i] += 1
+for x in range(n):
+    for y in range(n - k + 1):
+        o = m[y:y+k]
+        s = [o[q][x] for q in range(k)]
+        if not '#' in s:
+            for i in range(k):
+                v[y + i][x] += 1
+mx = 0
+my = 0
+m = v[0][0]
+for i in range(n):
+    for q in range(n):
+        if v[i][q] > m:
+            mx = q
+            my = i
+            m = v[i][q]
+print(my + 1, mx + 1)
