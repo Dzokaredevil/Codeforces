769cc36b1da4cfd392361ed81b665f0e44934102
Dmitrii Voitovich <dima.voytovich@mail.ru>
2018-04-29 10:44:09 +0300

    Update Messages.py
    B. Messages
    time limit per test1 second
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    There are n incoming messages for Vasya. The i-th message is going to be received after ti minutes. Each message has a cost, which equals to A initially. After being received, the cost of a message decreases by B each minute (it can become negative). Vasya can read any message after receiving it at any moment of time. After reading the message, Vasya's bank account receives the current cost of this message. Initially, Vasya's bank account is at 0.
    
    Also, each minute Vasya's bank account receives C·k, where k is the amount of received but unread messages.
    
    Vasya's messages are very important to him, and because of that he wants to have all messages read after T minutes.
    
    Determine the maximum amount of money Vasya's bank account can hold after T minutes.
    
    Input
    The first line contains five integers n, A, B, C and T (1 ≤ n, A, B, C, T ≤ 1000).
    
    The second string contains n integers ti (1 ≤ ti ≤ T).
    
    Output
    Output one integer  — the answer to the problem.

--- Messages.py
+++ Messages.py
@@ -1,6 +1,6 @@
 n, A, B, C, T = map(int, input().split())
 t = sum(list(map(int, input().split())))
 if C - B < 0:
-    print(n * A)
+    print(n * A) 
 else:
     print(n * A + (C - B) * (n * T - t))
