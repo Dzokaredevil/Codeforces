2a10d48e84e573952e095cd600255a43e8a1ec19
Dmitrii Voitovich <dima.voytovich@mail.ru>
2018-05-08 14:04:16 +0300

    Update Less or Equal.py
    C. Less or Equal
    time limit per test2 seconds
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    You are given a sequence of integers of length 
    n
     and integer number 
    k
    . You should print any integer number 
    x
     in the range of 
    [
    1
    ;
    10
    9
    ]
     (i.e. 
    1
    ≤
    x
    ≤
    10
    9
    ) such that exactly 
    k
     elements of given sequence are less than or equal to 
    x
    .
    
    Note that the sequence can contain equal elements.
    
    If there is no such 
    x
    , print "-1" (without quotes).
    
    Input
    The first line of the input contains integer numbers 
    n
     and 
    k
     (
    1
    ≤
    n
    ≤
    2
    ⋅
    10
    5
    , 
    0
    ≤
    k
    ≤
    n
    ). The second line of the input contains 
    n
     integer numbers 
    a
    1
    ,
    a
    2
    ,
    …
    ,
    a
    n
     (
    1
    ≤
    a
    i
    ≤
    10
    9
    ) — the sequence itself.
    
    Output
    Print any integer number 
    x
     from range 
    [
    1
    ;
    10
    9
    ]
     such that exactly 
    k
     elements of given sequence is less or equal to 
    x
    .
    
    If there is no such 
    x
    , print "-1" (without quotes).

--- Less or Equal.py
+++ Less or Equal.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 a = [int(i) for i in input().split()]
 a.sort()
 try:
-    if k==0 and a[0]>1: print(a[0]-1)
+    if k==0 and a[0]>1: print(a[0]-1) 
     elif a[k]-1>=a[k-1]: print(a[k]-1)
     else: print(-1)
 except IndexError:
