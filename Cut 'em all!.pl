1aea3066a3505fa57b4716ecfa20a0e489931566
Dzokaredevil <dima.voytovich@mail.ru>
2018-05-20 04:43:59 +0300

    Output a single integer k  — the maximum number of edges that can be removed to leave all connected components with even size, or − 1 if it is impossible to remove edges in order to satisfy this property.

---
+++ Cut 'em all!.pl
@@ -0,0 +1,29 @@
+$_ = <>;
+
+map {
+	( $u, $v ) = split ' ', <>;
+	$h{ $u }{ $v } ++;
+	$h{ $v }{ $u } ++;
+	} 2 .. $_;
+
+exit if $_ % 2 and print -1;
+
+@g = ( 1 ) x ( $_ + 1 );
+
+%N = %h;
+
+while( @_ = grep 1 == keys %{ $h{ $_ } }, keys %N ){
+					
+	undef %N;
+	
+	for( @_ ){
+		next if not $P = ( keys %{ $h{ $_ } } )[ 0 ];
+		$N{ $P } ++;
+		delete $h{ $P }{ $_ };
+		delete $h{ $_ };
+		
+		$g[ $_ ] % 2 ? $g[ $P ] += $g[ $_ ] : ++ $C;
+		}
+	}
+
+print 0 + $C
