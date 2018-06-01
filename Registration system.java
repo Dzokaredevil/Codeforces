aa977e5c4dd610f70ac2c8f6d7553c20070f1a90
Dzokaredevil <dima.voytovich@mail.ru>
2018-06-02 01:35:05 +0700

    Print n lines, which are system responses to the requests: OK in case of successful registration, or a prompt with a new name, if the requested name is already taken.

--- Registration system.java
+++ Registration system.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileWriter; 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
