82fc3dc4f3eefec3c3caa32898a5acdf8d8831e3
Dzokaredevil <dima.voytovich@mail.ru>
2018-06-04 09:59:31 +0700

    Print the required minimum number of shovels Polycarp has to buy so that he can pay for them without any change.

---
+++ Buy a Shovel.java
@@ -0,0 +1,165 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        int k = in.readInt();
+        int r = in.readInt();
+        for(int n = 1 ; n <= 10 ; n++) {
+            if((n * k - r) % 10 == 0 || ((n*k) % 10 == 0)) {
+                System.out.println(n);
+                return;
+            }
+        }
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1000];
+        private int curChar, numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuffer res = new StringBuffer();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private String readLine0() {
+            StringBuffer buf = new StringBuffer();
+            int c = read();
+            while (c != '\n' && c != -1) {
+                buf.appendCodePoint(c);
+                c = read();
+            }
+            return buf.toString();
+        }
+
+        public String readLine() {
+            String s = readLine0();
+            while (s.trim().length() == 0)
+                s = readLine0();
+            return s;
+        }
+
+        public String readLine(boolean ignoreEmptyLines) {
+            if (ignoreEmptyLines)
+                return readLine();
+            else
+                return readLine0();
+        }
+
+        public char readCharacter() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            return (char) c;
+        }
+
+        public double readDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, readInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, readInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+    }
+}
