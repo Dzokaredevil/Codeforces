e552e6086e62f764045fbfd9471b712a17551ec7
Dzokaredevil <dima.voytovich@mail.ru>
2018-06-09 00:49:22 +0300

    Print one such positive number without leading zeroes, — the answer to the problem, or  - 1, if such number doesn't exist. If there are multiple possible answers, you are allowed to print any of them.

---
+++ Olesya and Rodion.java
@@ -0,0 +1,343 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    InputReader in;
+    Printer out;
+
+    private void start() throws Exception {
+        int n = in.nextInt();
+        int t = in.nextInt();
+        
+        if (n == 1 && t == 10) {
+            out.println(-1);
+            
+            out.flush();
+            return;
+        }
+        
+        if (t == 10) {
+            for (int i = 0; i < n - 1; i++) {
+                out.print("" + 1);
+            }
+            out.println(0);
+            
+            out.flush();
+            return;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            out.print("" + t);
+        }
+        out.println();
+        
+        out.flush();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        InputReader in;
+        PrintStream out;
+
+        //in = new InputReader(new FileInputStream(new File("in.txt")));
+        //out = new PrintStream("out.txt");
+        in = new InputReader(System.in);
+        out = System.out;
+
+        Main main = new Main();
+
+        main.in = in;
+        main.out = new Printer(out);
+
+        main.start();
+    }
+
+    private static class InputReader {
+
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public double nextDouble() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E') {
+                    return res * Math.pow(10, nextInt());
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E') {
+                        return res * Math.pow(10, nextInt());
+                    }
+                    if (c < '0' || c > '9') {
+                        throw new InputMismatchException();
+                    }
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return nextString();
+        }
+
+        public interface SpaceCharFilter {
+
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    private static class Printer {
+
+        PrintStream out;
+        StringBuilder buffer = new StringBuilder();
+        boolean autoFlush;
+
+        public Printer(PrintStream out) {
+            this.out = out;
+        }
+
+        public Printer(PrintStream out, boolean autoFlush) {
+            this.out = out;
+            this.autoFlush = autoFlush;
+        }
+
+        public void println() {
+            buffer.append("\n");
+
+            if (autoFlush) {
+                flush();
+            }
+        }
+
+        public void println(int n) {
+            println(Integer.toString(n));
+        }
+
+        public void println(long n) {
+            println(Long.toString(n));
+        }
+
+        public void println(double n) {
+            println(Double.toString(n));
+        }
+
+        public void println(float n) {
+            println(Float.toString(n));
+        }
+
+        public void println(boolean n) {
+            println(Boolean.toString(n));
+        }
+
+        public void println(char n) {
+            println(Character.toString(n));
+        }
+
+        public void println(byte n) {
+            println(Byte.toString(n));
+        }
+
+        public void println(short n) {
+            println(Short.toString(n));
+        }
+
+        public void println(Object o) {
+            println(o.toString());
+        }
+
+        public void println(String s) {
+            buffer.append(s).append("\n");
+
+            if (autoFlush) {
+                flush();
+            }
+        }
+
+        public void print(String s) {
+            buffer.append(s);
+
+            if (autoFlush) {
+                flush();
+            }
+        }
+
+        public void flush() {
+            try {
+                BufferedWriter log = new BufferedWriter(new OutputStreamWriter(out));
+                log.write(buffer.toString());
+
+                log.flush();
+                buffer = new StringBuilder();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class Pair<K, V> implements Serializable {
+
+        private K key;
+
+        public K getKey() {
+            return key;
+        }
+        private V value;
+
+        public V getValue() {
+            return value;
+        }
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return key + "=" + value;
+        }
+
+        @Override
+        public int hashCode() {
+            return key.hashCode() * 13 + (value == null ? 0 : value.hashCode());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o instanceof Pair) {
+                Pair pair = (Pair) o;
+                if (key != null ? !key.equals(pair.key) : pair.key != null) {
+                    return false;
+                }
+                if (value != null ? !value.equals(pair.value) : pair.value != null) {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
