2db46d80d8ae5bfaa6e50fabf0d08a5da3c2b176
Dzokaredevil <dima.voytovich@mail.ru>
2018-06-23 00:55:46 +0300

    If Mishka is the winner of the game, print "Mishka" (without quotes) in the only line.
    If Chris is the winner of the game, print "Chris" (without quotes) in the only line.
    
    If the result of the game is draw, print "Friendship is magic!^^" (without quotes) in the only line.

---
+++ Mishka and Game.cpp
@@ -0,0 +1,9 @@
+#include<cstdio>
+
+int n, a, b, ans;
+
+int main() {
+	scanf("%d",&n);
+	while (n--) scanf("%d%d",&a,&b), ans+=(a>b)-(a<b);
+	puts(ans==0?"Friendship is magic!^^":ans>0?"Mishka":"Chris");
+}
