4fdc4f080c337e75ff840667b62bbb0aa6876f49
Dzokaredevil <dima.voytovich@mail.ru>
2018-07-06 01:14:08 +0300

    Output "First" if the first player wins and "Second" otherwise.

---
+++ Game.java
@@ -0,0 +1,166 @@
+import java.io.*;
+import java.util.*;
+
+public class A {
+	FastScanner in = new FastScanner(System.in);
+	PrintWriter out = new PrintWriter(System.out);
+
+	public void run() {	
+		int n1 = in.nextInt(), n2 = in.nextInt();
+		int k1 = in.nextInt(), k2 = in.nextInt();
+		if (n1 <= n2) System.out.println("Second");
+		else System.out.println("First");
+		out.close();
+	}
+
+	public static void main(String[] args) {
+		new A().run();
+	}
+
+	public void mapDebug(int[][] a) {
+		System.out.println("--------map display---------");
+
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.printf("%3d ", a[i][j]);
+			}
+			System.out.println();
+		}
+
+		System.out.println("----------------------------");
+		System.out.println();
+	}
+
+	public void debug(Object... obj) {
+		System.out.println(Arrays.deepToString(obj));
+	}
+
+	class FastScanner {
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+
+		public FastScanner(InputStream stream) {
+			this.stream = stream;
+			//stream = new FileInputStream(new File("dec.in"));
+
+		}
+
+		int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		boolean isEndline(int c) {
+			return c == '\n' || c == '\r' || c == -1;
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		int[] nextIntArray(int n) {
+			int[] array = new int[n];
+			for (int i = 0; i < n; i++)
+				array[i] = nextInt();
+
+			return array;
+		}
+
+		int[][] nextIntMap(int n, int m) {
+			int[][] map = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				map[i] = in.nextIntArray(m);
+			}
+			return map;
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		long[] nextLongArray(int n) {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++)
+				array[i] = nextLong();
+
+			return array;
+		}
+
+		long[][] nextLongMap(int n, int m) {
+			long[][] map = new long[n][m];
+			for (int i = 0; i < n; i++) {
+				map[i] = in.nextLongArray(m);
+			}
+			return map;
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		double[] nextDoubleArray(int n) {
+			double[] array = new double[n];
+			for (int i = 0; i < n; i++)
+				array[i] = nextDouble();
+
+			return array;
+		}
+
+		double[][] nextDoubleMap(int n, int m) {
+			double[][] map = new double[n][m];
+			for (int i = 0; i < n; i++) {
+				map[i] = in.nextDoubleArray(m);
+			}
+			return map;
+		}
+
+		String next() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isSpaceChar(c));
+			return res.toString();
+		}
+
+		String[] nextStringArray(int n) {
+			String[] array = new String[n];
+			for (int i = 0; i < n; i++)
+				array[i] = next();
+
+			return array;
+		}
+
+		String nextLine() {
+			int c = read();
+			while (isEndline(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isEndline(c));
+			return res.toString();
+		}
+	}
+}
