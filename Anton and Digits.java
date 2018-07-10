a3ac6a5671591d4bf12c4f9d82c19d093a4b0a37
Dmitrii Voitovich <dima.voytovich@mail.ru>
2018-07-11 00:33:05 +0300

    Update Anton and Digits.java
    Anton and Digits
    time limit per test1 second
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    Recently Anton found a box with digits in his room. There are k2 digits 2, k3 digits 3, k5 digits 5 and k6 digits 6.
    
    Anton's favorite integers are 32 and 256. He decided to compose this integers from digits he has. He wants to make the sum of these integers as large as possible. Help him solve this task!
    
    Each digit can be used no more than once, i.e. the composed integers should contain no more than k2 digits 2, k3 digits 3 and so on. Of course, unused digits are not counted in the sum.
    
    Input
    The only line of the input contains four integers k2, k3, k5 and k6 — the number of digits 2, 3, 5 and 6 respectively (0 ≤ k2, k3, k5, k6 ≤ 5·106).
    
    Output
    Print one integer — maximum possible sum of Anton's favorite integers that can be composed using digits from the box.

--- Anton and Digits.java
+++ Anton and Digits.java
@@ -3,7 +3,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashMap; 
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
