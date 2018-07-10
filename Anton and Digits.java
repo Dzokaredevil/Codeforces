9750112eed88ae0e3e8bfe7ad25d73c6e0482112
Dzokaredevil <dima.voytovich@mail.ru>
2018-07-11 00:31:30 +0300

    Print one integer — maximum possible sum of Anton's favorite integers that can be composed using digits from the box.

---
+++ Anton and Digits.java
@@ -0,0 +1,191 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+ 
+public class george
+{
+    static class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+ 
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+ 
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+ 
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1)
+            {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+ 
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+ 
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+ 
+            if (c == '.')
+            {
+                while ((c = read()) >= '0' && c <= '9')
+                {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+ 
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+ 
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+    static int min(int []arr,int n) {
+    int min=arr[0],k=-1;
+    for(int i=0;i<n;i++) {
+    	if(min>arr[i]) {
+    		k=arr[i];
+    	}
+    }
+    return k;
+    }
+    static int max(int []arr,int n) {
+        int max=arr[0],k=-1;
+        for(int i=1;i<n;i++) {
+        	if(arr[i]>max) 
+        		k=arr[i];
+        }
+        return k;
+        }
+static int gcd(int a,int b) {
+	if (b==0) return a;
+	return gcd(b,a%b);
+}
+static int lcm(int a,int b) {
+
+	return a*b/gcd(a,b);
+}
+static int tprime(int n) {
+int a=1;
+	for(int i=2;i<=n;i++) {
+		if(n%i==0) {
+			System.out.println(i);
+			a++;
+		}
+	}
+	return a;
+}
+
+    public static void main(String[] args) throws IOException
+    {
+    
+    Reader s=new Reader();
+//    Scanner in = new Scanner(System.in);
+    long k2 =s.nextInt();
+    long k3 =s.nextInt();
+    long k5 =s.nextInt();
+    long k6 =s.nextInt();
+    long min=Math.min(k2,Math.min(k5,k6));
+  long  ans=(min*256)+Math.min((k2-min),k3)*32;
+  System.out.println(ans);
+    }	
+}
