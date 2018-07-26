b153d75e2791f1a02013f0fd9ef125c0d97722db
Dmitrii Voitovich <dima.voytovich@mail.ru>
2018-07-27 00:36:09 +0300

    Update Grasshopper And the String.java
    Grasshopper And the String
    time limit per test1 second
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    One day, the Grasshopper was jumping on the lawn and found a piece of paper with a string. Grasshopper became interested what is the minimum jump ability he should have in order to be able to reach the far end of the string, jumping only on vowels of the English alphabet. Jump ability is the maximum possible length of his jump.
    
    Formally, consider that at the begginning the Grasshopper is located directly in front of the leftmost character of the string. His goal is to reach the position right after the rightmost character of the string. In one jump the Grasshopper could jump to the right any distance from 1 to the value of his jump ability.
    
    The picture corresponds to the first example.
    The following letters are vowels: 'A', 'E', 'I', 'O', 'U' and 'Y'.
    
    Input
    The first line contains non-empty string consisting of capital English letters. It is guaranteed that the length of the string does not exceed 100.
    
    Output
    Print single integer a — the minimum jump ability of the Grasshopper (in the number of symbols) that is needed to overcome the given string, jumping only on vowels.

--- Grasshopper And the String.java
+++ Grasshopper And the String.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.*;
-
+ 
 public class Main {
     public static void main(String[] args) throws IOException {
         InputReader in = new InputReader();
