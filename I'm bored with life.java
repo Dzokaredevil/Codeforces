df98d3e8fd5b7568ec622753df930621a0514ff6
Dzokaredevil <dima.voytovich@mail.ru>
2018-08-16 00:38:07 +0300

    Print a single integer denoting the greatest common divisor of integers A! and B!.

---
+++ I'm bored with life.java
@@ -0,0 +1,193 @@
+import java.io.*;
+import java.util.InputMismatchException;
+
+public final class ProblemA 
+{
+	private static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public int[] readIntArray(int n)
+        {
+        	int a[] = new int[n];
+        	for(int i=0;i<n;i++)
+        	{
+        		a[i] = readInt();
+        	}
+        	return a;
+        }
+        
+        public double[] readDoubleArray(int n)
+        {
+        	double a[] = new double[n];
+        	for(int i=0;i<n;i++)
+        	{
+        		a[i] = readDouble();
+        	}
+        	return a;
+        }
+        
+        public long[] readLongArray(int n)
+        {
+        	long a[] = new long[n];
+        	for(int i=0;i<n;i++)
+        	{
+        		a[i] = readLong();
+        	}
+        	return a;
+        }
+        
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int read()
+        {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                } catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+        
+        public double readDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, readInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, readInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+        
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+       
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    public static void main(String[] args) 
+	{
+		InputReader input = new InputReader(System.in);
+    	long a = input.readLong();
+    	long b = input.readLong();
+    	long min = Math.min(a, b);
+    	long result = 1;
+    	for(int i=1;i<=min;i++)
+    	{
+    		result*=i;
+    	}
+    	System.out.println(result);
+    	
+	}
+}
