4a488905abc52494da0bdef4b96ca3b87622cb91
Dzokaredevil <dima.voytovich@mail.ru>
2018-08-31 01:37:26 +0300

    Print a picture for the given n. You should strictly observe the number of spaces before the first digit on each line. Every two adjacent digits in the same line should be separated by exactly one space. There should be no spaces after the last digit at the end of each line.

---
+++ Present from Lena.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+public class PresentFromLena {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner (System.in);
+        int num=scan.nextInt();
+        switch (num) {
+            case 2:
+                System.out.println("    0");
+                System.out.println("  0 1 0");
+                System.out.println("0 1 2 1 0");
+                System.out.println("  0 1 0");
+                System.out.println("    0");
+                break;
+            case 3:
+                System.out.println("      0");
+                System.out.println("    0 1 0");
+                System.out.println("  0 1 2 1 0");
+                System.out.println("0 1 2 3 2 1 0");
+                System.out.println("  0 1 2 1 0");
+                System.out.println("    0 1 0");
+                System.out.println("      0");
+                break;
+            case 4:
+                System.out.println("        0");
+                System.out.println("      0 1 0");
+                System.out.println("    0 1 2 1 0");
+                System.out.println("  0 1 2 3 2 1 0");
+                System.out.println("0 1 2 3 4 3 2 1 0");
+                System.out.println("  0 1 2 3 2 1 0");
+                System.out.println("    0 1 2 1 0");
+                System.out.println("      0 1 0");
+                System.out.println("        0");
+                break;
+            case 5:
+                System.out.println("          0");
+                System.out.println("        0 1 0");
+                System.out.println("      0 1 2 1 0");
+                System.out.println("    0 1 2 3 2 1 0");
+                System.out.println("  0 1 2 3 4 3 2 1 0");
+                System.out.println("0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 3 2 1 0");
+                System.out.println("    0 1 2 3 2 1 0");
+                System.out.println("      0 1 2 1 0");
+                System.out.println("        0 1 0");
+                System.out.println("          0");
+                break;
+            case 6:
+                System.out.println("            0");
+                System.out.println("          0 1 0");
+                System.out.println("        0 1 2 1 0");
+                System.out.println("      0 1 2 3 2 1 0");
+                System.out.println("    0 1 2 3 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 3 2 1 0");
+                System.out.println("      0 1 2 3 2 1 0");
+                System.out.println("        0 1 2 1 0");
+                System.out.println("          0 1 0");
+                System.out.println("            0");
+                break;
+            case 7:
+                System.out.println("              0");
+                System.out.println("            0 1 0");
+                System.out.println("          0 1 2 1 0");
+                System.out.println("        0 1 2 3 2 1 0");
+                System.out.println("      0 1 2 3 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("0 1 2 3 4 5 6 7 6 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("      0 1 2 3 4 3 2 1 0");
+                System.out.println("        0 1 2 3 2 1 0");
+                System.out.println("          0 1 2 1 0");
+                System.out.println("            0 1 0");
+                System.out.println("              0");
+                break;
+            case 8:
+                System.out.println("                0");
+                System.out.println("              0 1 0");
+                System.out.println("            0 1 2 1 0");
+                System.out.println("          0 1 2 3 2 1 0");
+                System.out.println("        0 1 2 3 4 3 2 1 0");
+                System.out.println("      0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 6 7 6 5 4 3 2 1 0");
+                System.out.println("0 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 6 7 6 5 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("      0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("        0 1 2 3 4 3 2 1 0");
+                System.out.println("          0 1 2 3 2 1 0");
+                System.out.println("            0 1 2 1 0");
+                System.out.println("              0 1 0");
+                System.out.println("                0");
+                break;
+            case 9:
+                System.out.println("                  0");
+                System.out.println("                0 1 0");
+                System.out.println("              0 1 2 1 0");
+                System.out.println("            0 1 2 3 2 1 0");
+                System.out.println("          0 1 2 3 4 3 2 1 0");
+                System.out.println("        0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("      0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 5 6 7 6 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1 0");
+                System.out.println("0 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1 0");
+                System.out.println("  0 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1 0");
+                System.out.println("    0 1 2 3 4 5 6 7 6 5 4 3 2 1 0");
+                System.out.println("      0 1 2 3 4 5 6 5 4 3 2 1 0");
+                System.out.println("        0 1 2 3 4 5 4 3 2 1 0");
+                System.out.println("          0 1 2 3 4 3 2 1 0");
+                System.out.println("            0 1 2 3 2 1 0");
+                System.out.println("              0 1 2 1 0");
+                System.out.println("                0 1 0");
+                System.out.println("                  0");
+                break;
+            default:
+                break;
+        }
+        
+    }
+    
+}
